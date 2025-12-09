68b24180b740ae3f8f49761dd42b4f8fb4b4b2b9
Supraja-Koppisetty <kvsupraja2710@gmail.com>
2025-12-08 22:20:08 -0500

    Added hero section & hamburger menu for navbar

---
+++ js/menu.js
@@ -0,0 +1,38 @@
+const hamburgerButton = document.querySelector("#hamburger-menu");
+const navLinks = document.getElementById("nav-links");
+hamburgerButton.addEventListener("click", () => {
+  const isHidden = navLinks.classList.contains("hidden");
+  if (isHidden) {
+    hamburgerButton.innerHTML = "&times;";
+    navLinks.classList.add(
+      "flex-col",
+      "absolute",
+      "flex",
+      "items-center",
+      "w-full",
+      "top-full",
+      "bg-gray-800",
+      "space-y-8",
+      "py-6",
+      "left-0",
+      "md:flex"
+    );
+    navLinks.classList.remove("hidden");
+  } else {
+    hamburgerButton.innerHTML = "&#9776;";
+    navLinks.classList.remove(
+      "flex-col",
+      "absolute",
+      "flex",
+      "items-center",
+      "w-full",
+      "top-full",
+      "bg-gray-800",
+      "space-y-8",
+      "py-6",
+      "left-0",
+      "md:flex"
+    );
+    navLinks.classList.add("hidden");
+  }
+});
