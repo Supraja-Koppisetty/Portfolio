84ada04b928be25be2be8ca0d026f25f7d8feb1d
Supraja-Koppisetty <kvsupraja2710@gmail.com>
2026-01-07 23:35:28 -0500

    Fixed hamburger menu issues by adding the required js and css, removed overriding styles from app.css & index.css, added scroll-behavior for a smooth scroll

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,10 @@
+import { useState } from "react";
+
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false); // Placeholder for menu state
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <header className="sticky top-0 z-50 bg-gray-800">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 relative">
@@ -10,25 +16,46 @@ const Header = () => {
             &lt;Veera Supraja /&gt;
           </a>
           {/* Hamurger menu */}
-          <button className="text-white md:hidden text-2xl" id="hamburger-menu">
-            {"\u2630"}
+          <button
+            className="text-white md:hidden text-2xl"
+            id="hamburger-menu"
+            onClick={toggleMenu}
+          >
+            {isOpen ? "\u2715" : "\u2630"}
           </button>
-          <div className="hidden md:flex md:space-x-8" id="nav-links">
-            <a href="#about" className="hover:text-teal-400 transition-colors">
+          <div
+            className={`${
+              isOpen
+                ? "flex flex-col absolute top-16 left-0 w-full bg-gray-800 space-y-6 py-6 z-50 h-screen items-center"
+                : "hidden"
+            } md:flex md:space-x-8 md:static md:flex-row md:bg-transparent md:space-y-0 md:py-0 md:w-auto`}
+            id="nav-links"
+          >
+            <a
+              href="#about"
+              className="hover:text-teal-400 transition-colors"
+              onClick={toggleMenu}
+            >
               About
             </a>
-            <a href="#skills" className="hover:text-teal-400 transition-colors">
+            <a
+              href="#skills"
+              className="hover:text-teal-400 transition-colors"
+              onClick={toggleMenu}
+            >
               Skills
             </a>
             <a
               href="#projects"
               className="hover:text-teal-400 transition-colors"
+              onClick={toggleMenu}
             >
               Projects
             </a>
             <a
               href="#contact"
               className="hover:text-teal-400 transition-colors"
+              onClick={toggleMenu}
             >
               Contact
             </a>
